fd9d3486c19c9af09c5029cdc52bb03f7b13eff4
Axel-bug <125321228+Axel-bug@users.noreply.github.com>
2026-05-27 17:28:58 -0400

    diseño y funcionalidades

--- events/views.py
+++ events/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Event
 
+
 class EventListView(ListView):
     model = Event
     template_name = 'events/event_list.html'
     context_object_name = 'events'
 
     def get_queryset(self):
-        return Event.objects.filter(is_published=True).order_by('date_time')
+        # eventos públicos
+        return Event.objects.filter(
+            is_published=True
+        ).order_by('date_time')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # próximos eventos / ocultos
+        context['upcoming_events'] = Event.objects.filter(
+            is_published=False
+        ).order_by('date_time')
+
+        return context
+
 
 class EventDetailView(DetailView):
     model = Event
--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import buy_ticket, ticket_success, MyTicketsView, TicketScannerView, download_ticket_pdf
+from .views import buy_ticket, ticket_success, MyTicketsView, TicketScannerView, download_ticket_pdf, my_favorite
 from .api_views import ValidateTicketView
+from .views import toggle_favorite
 
 urlpatterns = [
     path('buy/<int:ticket_type_id>/', buy_ticket, name='buy_ticket'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('api/validate/', ValidateTicketView.as_view(), name='api_validate_ticket'),
     path('scan/', TicketScannerView.as_view(), name='ticket_scanner'),
     path('download/<uuid:ticket_id>/', download_ticket_pdf, name='download_ticket'),
+    
+#FAVORITOS
+    path('mis-favoritos/', my_favorite, name='my_favorite'),
+    path('toggle_favorite/', toggle_favorite, name='toggle_favorite')
 ]
