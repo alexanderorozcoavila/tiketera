86c712c6646abbde2fce34614a2368387f1460d3
Axel-bug <125321228+Axel-bug@users.noreply.github.com>
2026-06-15 17:23:40 -0400

    feat: implementar centro de ayuda y mejora de visualizacion de tickets y navegacion
    - Se agregó el Centro de Ayuda con formulario de contacto y envío de consultas por correo.
    - Se integraron vistas, URLs y lógica de backend para procesar solicitudes de soporte.
    - Se corrigió la visualización de mensajes de confirmación.
    - Se simplificó la información mostrada en los tickets para utilizar únicamente datos reales almacenados en el sistema.
    - Se eliminaron valores estáticos o ficticios del modal de tickets.
    - Se realizaron ajustes de interfaz y navegación relacionados con soporte y gestión de tickets.

--- events/views.py
+++ events/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from .models import Event
 
+#FORMULARIO DE AYUDA
+from django.core.mail import EmailMessage
+from django.contrib import messages
 
 class EventListView(ListView):
     model = Event
@@ -53,3 +56,56 @@ class AllFilterEventView(ListView):
             )
 
         return queryset
+    
+    
+# ==========================
+# CENTRO DE AYUDA
+# ==========================
+
+def help_view(request):
+    return render(request, 'events/help.html')
+
+
+def enviar_consulta(request):
+
+    if request.method == "POST":
+
+        nombre = request.POST.get('nombre')
+        correo = request.POST.get('correo')
+        telefono = request.POST.get('telefono')
+        mensaje = request.POST.get('mensaje')
+
+        cuerpo = f"""
+Nueva consulta desde FastiTicket
+
+Nombre: {nombre}
+Correo: {correo}
+Teléfono: {telefono}
+
+Mensaje:
+{mensaje}
+"""
+
+        email = EmailMessage(
+            subject=f"[FASTITICKET] Consulta de {nombre}",
+            body=cuerpo,
+            to=['correoEmpresa@gmail.com']  # cambia por coreo de empresa
+        )
+
+        email.reply_to = [correo]
+
+        try:
+            email.send()
+
+            messages.success(
+                request,
+                'Consulta enviada correctamente ⚡'
+            )
+
+        except Exception:
+            messages.error(
+                request,
+                'No fue posible enviar la consulta.'
+            )
+
+    return redirect('help')
--- tiketera/settings.py
+++ tiketera/settings.py
@@ -146,7 +146,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'correoEmpresa@gmail.com'
+EMAIL_HOST_PASSWORD = 'clave del correo de empresa' #CAMBIAR A CORREO DE EMPRESA Y PONERLO EN UN .ENV
 
 import os
 MEDIA_URL = '/media/'
